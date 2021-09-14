fc06330b61b51f0d0f842fc91d419aad54a22c0a
Cauã Menezes <cauamenezes.07@gmail.com>
2021-09-14 20:43:47 -0300

    first commit

---
+++ app/src/main/java/com/example/relativelayout/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var editNome: EditText
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("LIFE_CYCLE","OnResume")
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("LIFE_CYCLE","OnCreate")
+
+        val buttonOk = findViewById<Button>(R.id.button_ok)
+        editNome = findViewById<EditText>(R.id.edit_nome)
+        val editPassword = findViewById<EditText>(R.id.edit_password)
+
+        buttonOk.setOnClickListener {
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("LIFE_CYCLE","OnStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("LIFE_CYCLE","OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("LIFE_CYCLE","OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("LIFE_CYCLE","OnDestroy")
+    }
+
+
+    fun validar(){
+
+    }
+}
+
