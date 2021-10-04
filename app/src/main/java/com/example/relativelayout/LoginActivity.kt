b23cacc17534ac4c57def29527818a76996c6cc1
cauamenezes <cauamenezes.07@gmail.com>
2021-10-04 11:28:22 -0300

    layout de login alterado e de novo usuário sendo feita

--- app/src/main/java/com/example/relativelayout/LoginActivity.kt
+++ app/src/main/java/com/example/relativelayout/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.relativelayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val tvNovaConta = findViewById<TextView>(R.id.tv_nova_conta)
+
+        tvNovaConta.setOnClickListener {
+            val novaConta = Intent(this, NovoUsuarioActivity)
+            startActivity(novaConta)
+        }
     }
 }
