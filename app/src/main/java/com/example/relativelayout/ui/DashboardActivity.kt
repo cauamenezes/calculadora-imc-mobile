f58c81d0b1cd73856eda8cad0923fd352991d0c9
cauamenezes <cauamenezes.07@gmail.com>
2021-11-22 11:28:26 -0300

    fazendo tela de pesagem

--- app/src/main/java/com/example/relativelayout/ui/DashboardActivity.kt
+++ app/src/main/java/com/example/relativelayout/ui/DashboardActivity.kt
@@ -1,11 +1,15 @@
 package com.example.relativelayout.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.relativelayout.R
+import com.example.relativelayout.utils.calcularIdade
 import com.example.relativelayout.utils.convertBase64ToBitmap
 
 class DashboardActivity : AppCompatActivity() {
@@ -18,32 +22,44 @@ class DashboardActivity : AppCompatActivity() {
     lateinit var tvIdade: TextView
     lateinit var tvAltura: TextView
     lateinit var ivPerfil: ImageView
+    lateinit var buttonPesagem : RelativeLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        tvNome = findViewById(R.id.tv_dash_nome)
-        tvProfissao = findViewById(R.id.tv_dash_profissao)
-        tvImc = findViewById(R.id.tv_dash_imc)
-        tvNcd = findViewById(R.id.tv_dash_ncd)
-        tvPeso = findViewById(R.id.tv_dash_peso)
-        tvIdade = findViewById(R.id.tv_dash_idade)
-        tvAltura = findViewById(R.id.tv_dash_altura)
-        ivPerfil = findViewById(R.id.iv_dash_foto_perfil)
+        buttonPesagem = findViewById<RelativeLayout>(R.id.button_pesagem)
 
-        supportActionBar!!.hide()
+        buttonPesagem.setOnClickListener {
+            val pesagem = Intent(this, PesoActivity::class.java)
+            startActivity(pesagem)
 
-        carregarDashboard()
+            tvNome = findViewById(R.id.tv_dash_nome)
+            tvProfissao = findViewById(R.id.tv_dash_profissao)
+            tvImc = findViewById(R.id.tv_dash_imc)
+            tvNcd = findViewById(R.id.tv_dash_ncd)
+            tvPeso = findViewById(R.id.tv_dash_peso)
+            tvIdade = findViewById(R.id.tv_dash_idade)
+            tvAltura = findViewById(R.id.tv_dash_altura)
+            ivPerfil = findViewById(R.id.iv_dash_foto_perfil)
+
+            supportActionBar!!.hide()
+
+            carregarDashboard()
+
+        }
+
     }
 
     private fun carregarDashboard() {
         val arquivo = getSharedPreferences(
-            "usuario", MODE_PRIVATE)
+            "usuario", MODE_PRIVATE
+        )
 
         tvNome.text = arquivo.getString("nome", "")
         tvProfissao.text = arquivo.getString("profissao", "")
         tvAltura.text = arquivo.getFloat("altura", 0.0f).toString()
+        tvIdade.text = calcularIdade(arquivo.getString("dataNascimento", "")!!).toString()
 
         val bitmap = convertBase64ToBitmap(arquivo.getString("fotoPerfil", "")!!)
         ivPerfil.setImageBitmap(bitmap)
--- app/src/main/java/com/example/relativelayout/utils/Datas.kt
+++ app/src/main/java/com/example/relativelayout/utils/Datas.kt
@@ -1,6 +1,7 @@
 package com.example.relativelayout.utils
 
 import java.time.LocalDate
+import java.time.Period
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -13,4 +14,21 @@ fun convertStringToLocalDate(brazilDate: String) : LocalDate {
     parse(brazilDate, dateFormaterFromBrazil)
 
     return localDateFormat
+}
+
+fun calcularIdade(dataNascimento : String) : Int {
+
+    // Obter a data atual (hoje)
+    val hoje = LocalDate.now()
+
+    // Converter a data de nascimento em um LocalDate
+    // Extrair a data em um array
+    val nascimentoArray = dataNascimento.split("-").toTypedArray()
+    val nascimento = LocalDate.of(nascimentoArray[0].toInt(), nascimentoArray[1].toInt(), nascimentoArray[2].toInt())
+
+    // Obter a idade
+    val idade = Period.between(nascimento, hoje).years
+
+    return idade
+
 }
